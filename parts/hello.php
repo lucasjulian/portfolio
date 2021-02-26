1e5c5e717162b2c07b50a020eabbdeb4ad2bcaf7
Lucas Julian <lucasjulian-dev@outlook.fr>
2021-02-26 11:01:52 +0100

    feat(hello): design hello section

---
+++ parts/hello.php
@@ -0,0 +1,14 @@
+<section id="hero-hello" class="container flex">
+    <div id="introduce"  class="jc-center">
+        <span class="quote">Hello</span>
+        <h1 class="hero">I'M IBRAHIM SHAMSS</h1>
+        <h3 class="l-grey light-f">UI & UX Designer</h3>
+        <aside>
+            <a class="button" href="#">Hire me</a>
+            <a class="button link" href="#">Download CV Here<i class="fas fa-download"></i></a>
+        </aside>
+    </div>
+    <div id="me" class="jc-center">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/me.png" alt="">
+    </div>
+</section>
