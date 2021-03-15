d1da30bc479f7bb4afdc0930ebee7635dffe7a88
Lucas Julian <lucasjulian-dev@outlook.fr>
2021-03-15 23:29:27 +0100

    dyn(hero + what I do): dyn theses sections

--- parts/what-i-do.php
+++ parts/what-i-do.php
@@ -1,23 +1,30 @@
 <section id="about">
     <div class="container flex">
         <div id="interests"  class="jc-center">
+        <?php $args = array(  
+            'post_type' => 'interests',
+            'post_status' => 'publish',
+            'posts_per_page' => 3, 
+            'orderby' => 'title', 
+            'order' => 'ASC', 
+        );
+
+        $loop = new WP_Query( $args ); 
+            
+        while ( $loop->have_posts() ) : $loop->the_post(); ?>
             <div class="block-interest">
-                <img src="<?php echo get_theme_file_uri();?>/img/mobile-app-design.png" alt="">
-                <h4>Mobile App Design</h4>
-            </div> 
-            <div class="block-interest">
-                <img src="<?php echo get_theme_file_uri();?>/img/website-design.png" alt="">
-                <h4>Website Design</h4>
-            </div> 
-            <div class="block-interest">
-                <img src="<?php echo get_theme_file_uri();?>/img/prototyping.png" alt="">
-                <h4>Prototyping & wireframing</h4>
-            </div> 
+                <?php the_post_thumbnail(); ?>
+                <h4><?php the_title(); ?></h4>
+            </div>
+            
+            <?php endwhile;
+
+            wp_reset_postdata(); ?>
         </div>
         <div id="what-i-do" class="flex jc-center">
-            <span class="quote">what I do</span>
-            <h3>Better design,<br>Better experiences</h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium quaerat delectus molestiae quas laboriosam</p>
+            <span class="quote"><?php the_field('wid_quote'); ?></span>
+            <h3><?php the_field('wid_title'); ?></h4>
+            <p><?php the_field('wid_paragraph'); ?></p>
         </div>
     </div>
 </section>
