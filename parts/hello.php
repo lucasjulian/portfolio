6ee764e2adb89fdd24b889a86bc28fc0b7457385
Lucas Julian <lucasjulian-dev@outlook.fr>
2021-03-15 23:39:37 +0100

    dyn(home): hero hello and what i do sections

--- parts/hello.php
+++ parts/hello.php
@@ -1,14 +1,14 @@
 <section id="hero-hello" class="container flex">
     <div id="introduce"  class="jc-center">
-        <span class="quote">Hello</span>
-        <h1 class="hero">I'M IBRAHIM SHAMSS</h1>
-        <h3 class="l-grey light-f">UI & UX Designer</h3>
+        <span class="quote"><?php the_field('quote'); ?></span>
+        <h1 class="hero"><?php the_field('title'); ?></h1>
+        <h3 class="l-grey light-f"><?php the_field('sub_title'); ?></h3>
         <aside>
-            <a class="button" href="#">Hire me</a>
-            <a class="button link" href="#">Download CV Here<i class="fas fa-download"></i></a>
+            <a class="button" href="<?php the_field('link_button_hire'); ?>"><?php the_field('text_button_hire'); ?></a>
+            <a class="button link" href="<?php the_field('link_button'); ?>"><?php the_field('text_button'); ?><?php the_field('icon_button'); ?></a>
         </aside>
     </div>
     <div id="me" class="jc-center">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/me.png" alt="">
+        <img src="<?php echo the_field('illustration'); ?>" alt="">
     </div>
 </section>
